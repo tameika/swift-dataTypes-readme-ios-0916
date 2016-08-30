f2e7cf6973b6bd6af9b3cf7e984c0c1415da4f56
Tameika Lawrence <tameikalawrence@Tameikas-MacBook-Air.local>
2016-08-30 09:36:15 -0400

    dataTypes

---
+++ datatypes.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+let thirdPlanet = "Earth"
+
+var ninthPlanet: String
+ninthPlanet = "Pluto"
+
+let fifthPlanet: String = "Jupiter"
+
+let sixthPlanet: String = "Saturn"
+
+let seventhPlanet: String = "Uranus"
+
+let numberOfCountriesInAfrica = 54
+
+let numberOfMoonsOfSaturn = 62
+
+print("There are \(numberOfMoonsOfSaturn) moons orbiting \(sixthPlanet)")
+
+
+
